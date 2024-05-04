d6361137d239e7ff586f7d78f4ed55c9a0904e92
Tiagol125 <al75845@alunos.utad.pt>
2024-05-04 12:50:24 +0100

    firestore database

--- src/pages/marketPage.tsx
+++ src/pages/marketPage.tsx
@@ -1,10 +1,54 @@
+//@ts-nocheck
+import { useEffect, useState } from 'react';
+import { DocumentData, collection, onSnapshot, query, where } from 'firebase/firestore';
+
 import { Navbar } from '../components/navbar/navbar';
 import { TableMarket } from '../components/tableMarket/tableMarket';
+import db from '../firebase';
 
 export const MarketPage = () => {
+    const [cadernetastickers, setCadernetaStickers] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        const colletionRef = collection(db, 'Caderneta_Stickers');
+        const queryToDataBase = query(colletionRef, where('sticker', '>=', 'Artur'), where('sticker', '<=', 'Artur' + '\uf8ff'));
+
+        setIsLoading(true);
+        const unsub = onSnapshot(queryToDataBase, querySnapshot => {
+            const items: DocumentData[] = [];
+
+            querySnapshot.forEach(doc => {
+                items.push(doc.data());
+            });
+            setCadernetaStickers(items);
+            setIsLoading(false);
+        });
+
+        return () => {
+            unsub();
+        };
+    }, []);
+
     return (
         <>
             <Navbar />
+            {isLoading && (
+                <div className="spinner-border" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </div>
+            )}
+            {!isLoading && (
+                <ul className="list-group">
+                    {cadernetastickers.map(card => {
+                        return (
+                            <li key={card.sticker} className="list-group-item">
+                                {card.sticker}
+                            </li>
+                        );
+                    })}
+                </ul>
+            )}
             <div className="container">
                 <TableMarket />
             </div>
