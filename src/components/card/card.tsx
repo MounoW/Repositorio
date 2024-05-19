be22826356e8e07d379be5c3566e9e8a13bece7b
MounoW <al75746@alunos.utad.pt>
2024-05-19 21:06:42 +0100

    all people added

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -14,27 +14,29 @@ interface CardProps {
 
 export const Card = ({ nome, raridade, imagem }: CardProps) => {
     // eslint-disable-next-line @typescript-eslint/no-shadow
-    const getBorderColor = (raridade: string) => {
-        if (raridade === 'Comum') {
-            return 'gray';
-        } else if (raridade === 'Raro') {
-            return 'blue';
-        } else if (raridade === 'Muito Raro') {
-            return 'green';
-        } else if (raridade === 'Épico') {
-            return 'purple';
-        } else if (raridade === 'Lendário') {
-            return 'gold';
+    const getColors = (raridade: string) => {
+        switch (raridade) {
+            case 'Comum':
+                return { borderColor: 'gray', textColor: 'gray' };
+            case 'Raro':
+                return { borderColor: 'blue', textColor: 'blue' };
+            case 'Muito Raro':
+                return { borderColor: 'green', textColor: 'green' };
+            case 'Épico':
+                return { borderColor: 'purple', textColor: 'purple' };
+            case 'Lendário':
+                return { borderColor: 'gold', textColor: 'gold' };
+            default:
+                return { borderColor: 'white', textColor: 'white' };
         }
-
-        return 'white';
     };
 
-    const borderColor = getBorderColor(raridade);
+    const { borderColor, textColor } = getColors(raridade);
 
     return (
         <div className="card card-spacing card-size" style={{ border: `8px solid ${borderColor}` }}>
             <img
+                style={{ paddingTop: '20px' }}
                 src={
                     imagem ||
                     'https://st3.depositphotos.com/9998432/13335/v/450/depositphotos_133352010-stock-illustration-default-placeholder-man-and-woman.jpg'
@@ -44,7 +46,17 @@ export const Card = ({ nome, raridade, imagem }: CardProps) => {
             />
             <div className="card-body text-center">
                 <h3 className="card-title">{nome}</h3>
-                <h6 className="card-title">{raridade}</h6>
+                <h6
+                    className="card-title"
+                    style={{
+                        color: textColor,
+                        textShadow:
+                            '1px 1px 0 #000, -1px -1px 0 #000, -1px 1px 0 #000, 1px -1px 0 #000, 1px 0 0 #000, 0 1px 0 #000, -1px 0 0 #000, 0 -1px 0 #000',
+                        fontSize: '25px'
+                    }}
+                >
+                    {raridade}
+                </h6>
             </div>
             {/* Colocar um if para o botao so aparecer caso o utilizador tenha 2 ou mais iguais */}
             <div className="text-center">
